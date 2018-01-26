3c0e468be8554fd4eb10469bd20a0998cdf1f06c
kyo <kyo_liu@htc.com>
2018-01-26 17:10:30 +0800

    add new CustomCaller.cs  way to add controller

--- CodedUITestProject2/CodedUITestProject2/CodedUITest1.cs
+++ CodedUITestProject2/CodedUITestProject2/CodedUITest1.cs
@@ -30,7 +30,7 @@ namespace CodedUITestProject2
         public void CodedUITestMethod1()
         {
             // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
-            this.UIMap.RecordedMethod1();
+            //this.UIMap.RecordedMethod1();
             //csv方法
             /*Console.WriteLine(TestContext.DataRow["language"].ToString());
             byte[] bytes = Encoding.Default.GetBytes(TestContext.DataRow["text"].ToString());
@@ -39,7 +39,8 @@ namespace CodedUITestProject2
             //XMl方法 如果寫兩組整個流程會跑兩次...
             //Console.WriteLine(TestContext.DataRow["language"].ToString());
             //this.UIMap.AssertMethod1();
-
+            CustomCaller a= new CustomCaller();
+            a.Buttomclick();
         }
 
         #region Additional test attributes
